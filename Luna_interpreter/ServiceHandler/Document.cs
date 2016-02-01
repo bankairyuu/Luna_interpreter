cbc03d819b519053babdfc32f731739b7e0eadbc
bankairyuu <bankairyuu@gmail.com>
2016-02-01 15:51:14 +0100

    ~ adsf

--- Luna_interpreter/ServiceHandler/Document.cs
+++ Luna_interpreter/ServiceHandler/Document.cs
@@ -8,6 +8,47 @@ namespace Luna_interpreter.Model.Structure.Classes
 {
     class Document
     {
+        /*
 
+        try
+                {
+                    var doksi = client.GetTemplateDocuments();
+                    Console.WriteLine("doksik száma: " + doksi.Length);
+
+                    for (int i=0; i<doksi.Length; i++)
+                    {
+                        Console.WriteLine("Doksi neve: " + doksi[i].Name + " id: " + doksi[i].Id + " leírás: " + doksi[i].Description);
+
+                        var szekciók = client.GetTemplateDocumentSections(doksi[i].Id);
+                        Console.WriteLine("- A dokumentum szekcióinak száma: " + szekciók.Length);
+                        for (int j = 0; j<szekciók.Length; j++)
+                        {
+                            Console.WriteLine("\t" + szekciók[j].Name);
+                            try
+                            {
+                                Console.WriteLine("\t- adott szekció fildjeinek száma: " + szekciók[j].Fields.Length);
+
+                                for (int k = 0; k < szekciók[j].Fields.Length; k++)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\t\tA field neve: " + szekciók[j].Fields[k].Name);
+                                    }
+                                    catch (Exception exc) { }
+                                }
+
+                            }
+                            catch (Exception Exc) { }
+                        }
+                    }
+
+                    return doksi[0].Name;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+
+        */
     }
 }
--- Luna_interpreter/ServiceHandler/ServiceHandler.cs
+++ Luna_interpreter/ServiceHandler/ServiceHandler.cs
@@ -34,44 +34,8 @@ namespace Luna_interpreter.ServiceHandler
 
 
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~");
-                try
-                {
-                    var doksi = client.GetTemplateDocuments();
-                    Console.WriteLine("doksik száma: " + doksi.Length);
+                
 
-                    for (int i=0; i<doksi.Length; i++)
-                    {
-                        Console.WriteLine("Doksi neve: " + doksi[i].Name + " id: " + doksi[i].Id + " leírás: " + doksi[i].Description);
-
-                        var szekciók = client.GetTemplateDocumentSections(doksi[i].Id);
-                        Console.WriteLine("- A dokumentum szekcióinak száma: " + szekciók.Length);
-                        for (int j = 0; j<szekciók.Length; j++)
-                        {
-                            Console.WriteLine("\t" + szekciók[j].Name);
-                            try
-                            {
-                                Console.WriteLine("\t- adott szekció fildjeinek száma: " + szekciók[j].Fields.Length);
-
-                                for (int k = 0; k < szekciók[j].Fields.Length; k++)
-                                {
-                                    try
-                                    {
-                                        Console.WriteLine("\t\tA field neve: " + szekciók[j].Fields[k].Name);
-                                    }
-                                    catch (Exception exc) { }
-                                }
-
-                            }
-                            catch (Exception Exc) { }
-                        }
-                    }
-
-                    return doksi[0].Name;
-                }
-                catch (Exception exc)
-                {
-                    Console.WriteLine(exc.Message);
-                }
 
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~");
             }
